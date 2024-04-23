f21556b8a85caea4b227cf763103039c0a54de1e
sheikh-hammad <hammadusman001@gmail.com>
2024-04-23 09:41:21 +0500

    small changes

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
 import chalk from "chalk";
-
-const answer: {Sentence: string} = await inquirer.prompt([
+const answer: { Sentence: string } = await inquirer.prompt([
   {
     name: "Sentence",
     message: chalk.blue("Write your sentence to count words"),
     type: "input",
   },
 ]);
-const sentence = answer.Sentence.trim().split(" ")
+const sentence = answer.Sentence.trim().split(" ");
 
 // Print the array to the world to the console
 console.log(chalk.cyanBright.bold(`Total words are: ${sentence.length}`));
